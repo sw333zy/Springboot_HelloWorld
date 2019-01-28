88dc7b28e5df0d8df10071da129074aa339b8e99
sw333zy@gmail.com <GBTC440004UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-01-28 16:26:47 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Hello World!");
+
+        int num  = 0;
+
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        num = keyboard.nextInt();
+        System.out.println(num);
+
+    }
+}
